eaa1b1f80ee6b8254b3281124e64f9f5ef531786
Anand Raj <anandraj36401@gmail.com>
2025-04-03 17:15:48 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,21 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+// 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// 	theme: {
+// 		extend: {},
+// 	},
+// 	// eslint-disable-next-line no-undef
+// 	plugins: [require("daisyui")],
+// };
+
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	// eslint-disable-next-line no-undef
-	plugins: [require("daisyui")],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
 };
+
