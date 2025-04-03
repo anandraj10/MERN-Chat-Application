afbbaa44f4ab3646a618bc58bc856a9664d0dab5
Anand Raj <anandraj36401@gmail.com>
2025-04-03 15:57:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/home/home';
-import Login from './pages/login/login';
-import SignUp from './pages/signup/signup';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import SignUp from './pages/signup/Signup';
 import { Toaster } from 'react-hot-toast'
 import { useAuthContext } from './context/AuthContext';
 
